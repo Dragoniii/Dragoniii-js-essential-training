46f2ead92e54d061dadea0ec60e0907210b9bee2
Joel Luis Steffen <jo.lsteffen@gmail.com>
2023-10-03 11:30:28 -0300

    Ok

---
+++ DOM/sidebar.js
@@ -0,0 +1,21 @@
+function Camera(brand, model, year, format, lens, filmType) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.format = format;
+    this.lens = lens;
+    this.filmType = filmType;
+}
+
+const myCamera = new Camera("Hasselblad", "500C/M", 1963, "medium format", "Carl Zeiss 80mm f/2.8 Planar T", "120");
+
+const getCurrentYear = () => new Date().getFullYear();
+
+const cameraAge = (year) => getCurrentYear() - year;
+
+const cameraHTML = (myCamera) => {
+    const cameraStory = `My camera is a ${myCamera.brand} ${myCamera.model} made in ${myCamera.year} making it ${cameraAge(myCamera.year)} years old. It's a ${myCamera.format} camera with a ${myCamera.lens} lens using ${myCamera.filmType} film.`;
+    return cameraStory;
+}    
+
+document.body.innerHTML = `My camera is a ${myCamera.brand} ${myCamera.model} made in ${myCamera.year} making it ${cameraAge(myCamera.year)} years old. It's a ${myCamera.format} camera with a ${myCamera.lens} lens using ${myCamera.filmType} film.`;
